3bbe50a969c7575f3bfc39f41d0a6e47ec27d10c
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-10-19 17:00:51 +0900

    Create always_on_top.py

---
+++ always_on_top.py
@@ -0,0 +1,14 @@
+
+
+
+from tkinter import *
+
+test= Tk()
+test.geometry("600x350")
+
+Label(test, text= "test ",font=('Helvetica bold', 15)).pack(pady=20)
+test.attributes('-topmost',True)
+
+test.mainloop()
+
+
